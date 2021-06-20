bab2d830fd3ac4819d441a5884ef7fca8d388c4f
mariana.casetta.az <mariana.casetta.az@gmail.com>
2021-06-20 14:55:42 -0300

    ✨ add business in api and json data layer

--- src/controllers/currencyController.js
+++ src/controllers/currencyController.js
@@ -1,17 +1,33 @@
-const axios = require('axios');
+const currencyBusiness = require('../business/currencyBusiness');
+const coinsData = require('../util/coinsData.json');
+
 
 async function findOne(req, res, next) {
-    const data = {
-        coin: parseInt(req.query.coin),
-        valueConvert: parseInt(req.params.value) ?? null
-    };
+    // "if" for parameter coin does not exist, because if not entered it will not fall in this route
+    if (!req.query.value) {
+        return res.status(500).json({ error: "Param 'value' not found." });
+    }
 
+    // coin parameter is not being "treated" because it is compared in business
+    const data = {
+        coin: req.params.coin,
+        valueConvert: req.query.value
+    };
+    
     try {
-        const response = await axios.get('https://economia.awesomeapi.com.br/json/all');
+        const result = await currencyBusiness.findOne(data);
         
-        res.status(200).json(response.data);
+        if (result && !result.error) 
+            res.status(200).json({ result: result })
+        else if(result.error != '')
+            res.status(404).json({ result: result })
+        else
+            res.status(404).json({ error: "Error converting your currency, try again!" })
+
     } catch (e) {
-        console.log(e.response.body);
+        next(e);
+        return res.status(404).json({ error: "Error converting your currency, try again!" });
+
     }
 }
 
