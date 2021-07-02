e39242424069af119e95642995da9844d01e4dc5
mariana.casetta.az <mariana.casetta.az@gmail.com>
2021-07-02 16:42:20 -0300

    test improvements

--- src/tests/currentBusiness.test.js
+++ src/tests/currentBusiness.test.js
@@ -3,10 +3,26 @@ const currentBusiness = require('../business/currencyBusiness');
 describe('Currency Business Rules', () => {
     describe("Business answer", () => {
         it('should return data', async () => {
-            const value = 2; 
+            const value = "2"; 
             const response = await currentBusiness.findOne(value);
             const aux = Array.isArray(response);
             expect(aux).toBe(true);
+        });
+
+        it('should return message insert integer', async () => {
+            const value = "oi";
+            const response = await currentBusiness.findOne(value);
+
+            const objectExpect = {"message": "Insert values integers"};
+            expect(response).toMatchObject(objectExpect);
+        });
+        
+        it('should return value convert to point', async () => {
+            const value = "2,3";
+            const response = await currentBusiness.findOne(value);
+
+            expect(response[0].value).not.toBeNaN();
+            // expect(response[0].value).toMatch('/./');
         });
       });
 });
